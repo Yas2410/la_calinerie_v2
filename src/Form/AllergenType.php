c734336d49fab6769493b1c2d1ebd3683b165e1c
Yas2410 <yasemin.kocak@lapiscine.pro>
2020-05-18 13:17:18 +0200

    sass/css update

--- src/Form/AllergenType.php
+++ src/Form/AllergenType.php
@@ -3,8 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Allergen;
+use phpDocumentor\Reflection\Type;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +16,12 @@ class AllergenType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('type')
-            ->add('submit', SubmitType::class);
+            ->add('type', TextType::class, [
+                'label' => 'Type  '
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
